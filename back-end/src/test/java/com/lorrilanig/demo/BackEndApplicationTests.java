1e2295f7a6aba032bdc0162c0a2e35ba34f1ce04
Lorri Lanig <lorrilanig.art@gmail.com>
2024-12-14 13:09:54 -0600

    file structure

---
+++ back-end/src/test/java/com/lorrilanig/demo/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorrilanig.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
