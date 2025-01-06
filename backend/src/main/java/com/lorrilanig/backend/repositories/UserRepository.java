4b23ac7ec5f1b651441b75f0b6d350fca226a52f
Lorri Lanig <lorrilanig.art@gmail.com>
2025-01-06 14:32:32 -0600

    user model complete

--- backend/src/main/java/com/lorrilanig/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/lorrilanig/backend/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.lorrilanig.backend.repositories;
 
-public class UserRepository {
+import com.lorrilanig.backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
